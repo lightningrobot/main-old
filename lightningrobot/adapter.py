429119e80ee3c3fd537803df4ec637a28e1e969d
MeowSauce <meowsauce@hotmail.com>
2024-03-13 00:29:38 +0800

    新增 输出接受和发送消息。

--- lightningrobot/adapter.py
+++ lightningrobot/adapter.py
@@ -1,14 +1,16 @@
 from abc import ABC, abstractmethod
-
+from lightningrobot import log
 class Adapter(ABC):
     @abstractmethod
     async def connect(self) -> None:
         """连接到聊天平台"""
+        log.info("正在连接聊天平台...")
         pass
     
     @abstractmethod
     async def send_message(self, event_type, message: str) -> None:
         """发送消息"""
+        log.info("发送消息：",message)
         pass
 
     @abstractmethod
